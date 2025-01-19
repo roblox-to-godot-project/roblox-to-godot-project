2c0c2cb171a132722f6a28193943937e1e4a33a0
Brainstorm <87519518+Brainstorm4266@users.noreply.github.com>
2025-01-20 00:08:41 +0100

    delete a test file

--- roblox-to-godot-project-derive/schtuff.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Instance)]
-#[instance(hierarchy=[ServiceProvider])]
-#[method(some_method = "method_name")]
-struct UrStruct {
-    #[property(name = "YourPropertyName")]
-    property_name: PropertyType
-}
